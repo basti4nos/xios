02f445c38416e5c963d15e3a782b7ea912368643
huzitatu <huzitatu@users.noreply.github.com>
2018-07-07 15:39:11 +0900

    Add "xios-tester" module

---
+++ src/test/tester/InstanceTester.hpp
@@ -0,0 +1,33 @@
+/*============================================================================*/
+/*
+ *                     XIOS - http://metasophia.org/xios
+ *
+ *  This file is part of the XIOS, distributed under the ISC License.
+ *  Please see the LICENSE.txt file for details on copyright and licensing.
+ *
+ *============================================================================*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <Poco/Util/Application.h>
+#include <Poco/Util/OptionSet.h>
+
+namespace xios{
+
+using Poco::Util::Application;
+using Poco::Util::OptionSet;
+
+class InstanceTester: public Application{
+public:
+  InstanceTester();
+protected:
+  void initialize( Application& self );
+  void uninitialize();
+  void defineOptions( OptionSet& opts );
+  void handleOption( const std::string& name, const std::string& value );
+  int main( const std::vector< std::string >& args );
+};
+
+} // namespace xios
